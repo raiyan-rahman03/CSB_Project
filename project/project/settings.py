0af6575216c8e05bfaf5ab4cca1342d9942843ab
Black 2.o <black2.o@yahoo.com>
2024-08-21 01:04:56 +0600

    Added New Colro To The Website And Added New Font And Some Changes

--- project/project/settings.py
+++ project/project/settings.py
@@ -90,16 +90,28 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hackathon',
-        'USER': 'root',
-        'PASSWORD': 'root123',
-        'HOST': 'localhost',   # Or your MySQL server's IP address
-        'PORT': '3306',        # Default MySQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'hackathon',
+#         'USER': 'root',
+#         'PASSWORD': 'root123',
+#         'HOST': 'localhost',   # Or your MySQL server's IP address
+#         'PORT': '3306',        # Default MySQL port
+#     }
+# }
 
 
 
